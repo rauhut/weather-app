dfb3f15e9542d24495d975e2233b571efc320329
rauhut <jackierauhut@gmail.com>
2020-06-01 16:30:10 -0400

    changing response

--- back-end/server.js
+++ back-end/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("it is working!");
+});
+
 app.get("/weather", (req, res) => {
   weather.getWeather(req, res);
 });
